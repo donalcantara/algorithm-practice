488a641091883f47aa704a8268b418e716d4a46b
Don <donroyce@yahoo.com>
2016-05-02 12:12:57 -0700

    first commit

---
+++ AlgoPractice.js
@@ -0,0 +1,23 @@
+// console log all numbers from 1-55
+for (var i = 0; i < 256; i++) {
+	console.log(i);
+}
+
+// console log all odd numbers from 1-1000
+// for (var i = 0; i < 1001; i++) {
+// 	if (i % 2 != 0){
+// 		console.log(i);
+// 	}
+// }
+
+// print the sum of all the odd numbers from 1 to 5000
+// sum = 0;
+// for (var i = 0; i < 5001; i++) {
+// 	if (i % 2 != 0){
+// 	sum = sum + i;
+// 	}
+// }
+// console.log(sum);
+
+// x = [1,3,5,7,9,13]
+
